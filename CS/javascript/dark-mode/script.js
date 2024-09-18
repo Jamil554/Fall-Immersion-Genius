be67e5cc68d4132376f0d01c069584a9f434463c
Jamil554 <jamil.lewis@hgs.hiddengeniusproject.org>
2024-09-17 18:54:27 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,11 @@
 function darkMode() {
-// Your code here.
+if (document.body.style.backgroundColor === "white") {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    }
+    
 }
